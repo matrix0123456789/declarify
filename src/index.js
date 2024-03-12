7178bed7c311ce89ed0523f648e6497a697b2f7d
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-03-12 20:34:14 +0100

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {DeclarifyHandler} from "./declarifyHandler.js";
 import {DeclarifyScopeManager} from "./declarifyScopeManager.js";
 
 export function declarify(target){
-    let symbol=Symbol(DeclarifyScopeManager.currentScopeKey.description+'.[declarify]');
+    let symbol=Symbol(DeclarifyScopeManager.currentScopeKey.description+'.[declarify_'+Math.floor(Math.random()*0xffff).toString(16)+']');
     return new Proxy(target, new DeclarifyHandler(symbol));
 }
 export * from "./declarifyScopeManager.js"
